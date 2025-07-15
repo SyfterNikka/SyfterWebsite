73228ac44b95a421fd59676a6fa40436696abfac
SyfterNikka <nikka@syfter.com>
2025-07-15 11:45:32 -0600

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* CountUp.js via CDN */}
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.6.2/countUp.umd.min.js"
+          async
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
