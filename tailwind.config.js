d2b92714c8188fab2ffd9cfd814ac760be1183ea
SyfterNikka <nikka@syfter.com>
2025-07-16 10:50:29 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scrollUp: {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(-100%)' },
+        },
+      },
+      animation: {
+        scrollUp: 'scrollUp 10s linear infinite',
+      },
+      colors: {
+        syfterBlue: '#69bdff',
+        darkBlue: '#1e3a5f',
+        midBlue: '#3b82f6',
+      },
+    },
   },
   plugins: [],
 };
