9f77d6bd8b67fa47ba1cf902ae63879dad83dc5c
SyfterNikka <nikka@syfter.com>
2025-07-17 19:38:19 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 1s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateY(20px) scale(0.98)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateY(0) scale(1)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
